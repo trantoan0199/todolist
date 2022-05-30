5e1412931834cd46c5fd7eb7d92ee2e0a8b12131
hieudn2021 <hieu.dn2012@gmail.com>
2022-05-30 22:15:28 +0700

    Add firebase auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import { Box, ThemeProvider, createTheme } from "@mui/material"
+import React from "react"
+import {
+  Box,
+  ThemeProvider,
+  createTheme,
+  Button,
+  Typography
+} from "@mui/material"
 import Filters from "components/Filters"
 import FormTable from "components/FormTable"
 import { useState } from "react"
+
+import withFirebaseAuth from "react-with-firebase-auth"
+import firebaseConfig from "./firebaseConfig"
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+import "firebase/compat/database"
+import "firebase/compat/storage"
+
 import "./App.css"
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseAppAuth = firebaseApp.auth()
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider()
+}
 
 const theme = createTheme({
   typography: {
@@ -14,7 +36,10 @@ const theme = createTheme({
   }
 })
 
-function App() {
+function App(props) {
+  const { user, signOut, signInWithGoogle } = props
+
+  console.log(props, "props")
   const [open, setOpen] = useState(false)
   const [dataEdit, setDataEdit] = useState({})
   const handleOpen = () => {
@@ -26,21 +51,68 @@ function App() {
     setDataEdit({})
   }
 
+  const handleLogin = () => {
+    signInWithGoogle()
+  }
+
+  const handleLogout = () => {
+    signOut()
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Box maxWidth={1000} margin="auto" px={2} mt={10}>
-        <Filters
-          dataEdit={dataEdit}
-          setDataEdit={setDataEdit}
-          open={open}
-          setOpen={setOpen}
-          handleOpen={handleOpen}
-          handleClose={handleClose}
-        />
-        <FormTable handleOpen={handleOpen} setDataEdit={setDataEdit} />
+        {user ? (
+          <Button onClick={handleLogout} variant="outlined">
+            Logout
+          </Button>
+        ) : (
+          <Button onClick={handleLogin} variant="contained">
+            Login
+          </Button>
+        )}
+
+        {user && (
+          <React.Fragment>
+            <Box mt={2}>
+              <img
+                src={user?.multiFactor?.user?.photoURL}
+                alt="avatar"
+                style={{ width: 60, height: 60, borderRadius: "50%" }}
+              />
+            </Box>
+            <Box mt={2}>
+              <Typography variant="button">{`Hello: ${user?.multiFactor?.user?.displayName}`}</Typography>
+            </Box>
+            <Box>
+              <Typography variant="caption">{`Email: ${user?.multiFactor?.user?.email}`}</Typography>
+            </Box>
+          </React.Fragment>
+        )}
+
+        {user ? (
+          <React.Fragment>
+            <Filters
+              dataEdit={dataEdit}
+              setDataEdit={setDataEdit}
+              open={open}
+              setOpen={setOpen}
+              handleOpen={handleOpen}
+              handleClose={handleClose}
+            />
+            <FormTable handleOpen={handleOpen} setDataEdit={setDataEdit} />
+          </React.Fragment>
+        ) : (
+          <Typography variant="h6" display="block" sx={{ mt: 2 }}>
+            Please login with google authentication
+          </Typography>
+        )}
       </Box>
     </ThemeProvider>
   )
 }
 
-export default App
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+})(App)
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD5eUKFnu4nDJF3QwIAX2l72eUQW5qn3lw",
+  authDomain: "auth-d3a51.firebaseapp.com",
+  projectId: "auth-d3a51",
+  storageBucket: "auth-d3a51.appspot.com",
+  messagingSenderId: "33985416365",
+  appId: "1:33985416365:web:5f8b7795f94aa8dcec4a24",
+  measurementId: "G-R56674QFR2"
+}
+
+export default firebaseConfig
